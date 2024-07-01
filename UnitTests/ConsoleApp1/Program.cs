f0a72c1db2b09f1a8048a8efa8d451508c04dd45
rupacepro <rup.ace.pasa@gmail.com>
2024-07-01 14:11:21 -0400

    7.1

--- UnitTests/ConsoleApp1/Program.cs
+++ UnitTests/ConsoleApp1/Program.cs
@@ -1,36 +1,5 @@
-﻿Solution solution = new Solution();
-int[] result = solution.AsteroidCollision(new int[] { 5, 10, -5 });
-
-foreach (int i in result)
-{
-    Console.WriteLine(i);
-}
-
-public class Solution
-{
-    public int[] AsteroidCollision(int[] asteroids)
-    {
-        Stack<int> items = new Stack<int>();
-        int current, last = 0;
-        int n = asteroids.Length;
-        for (int i = 0; i < n; i++)
-        {
-            current = asteroids[i];
-            if (items.Count == 0 || (current > 0 && last > 0))
-            {
-                items.Push(current);
-                last = items.Peek();
-                continue;
-            }
-            last = items.Peek();
-            if (Math.Abs(current) > Math.Abs(last))
-            {
-                items.Pop();
-                items.Push(current);
-            }
-        }
-        int[] result = items.ToArray();
-        Array.Reverse(result);
-        return result;
-    }
-}
+﻿Stack<int> stk = new Stack<int>();
+stk.Push(0);
+stk.Push(1);
+stk.Push(2);
+Console.WriteLine(stk.Pop());
