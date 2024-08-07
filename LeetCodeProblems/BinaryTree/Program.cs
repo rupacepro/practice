6fc3b0df9473317aae92cbbc05243dd556bf6803
rupacepro <rup.ace.pasa@gmail.com>
2024-08-06 23:31:01 -0400

    8.6

---
+++ LeetCodeProblems/BinaryTree/Program.cs
@@ -0,0 +1,36 @@
+﻿public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class BinaryTree
+{
+    private TreeNode root;
+    public BinaryTree()
+    {
+        root = null;
+    }
+
+    public void Insert(int val)
+    {
+        if(root == null)
+        {
+            root = new TreeNode(val);
+        }
+        else
+        {
+            while(root != null)
+            {
+                root = root.left;
+            }
+        }
+    }
+}
