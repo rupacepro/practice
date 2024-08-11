74bc9da637a21ccdbffbb6b9c64ba44b5f04be7c
rupacepro <rup.ace.pasa@gmail.com>
2024-08-10 23:43:02 -0400

    8.10

--- LeetCodeProblems/BinaryTree/Program.cs
+++ LeetCodeProblems/BinaryTree/Program.cs
@@ -1,4 +1,16 @@
-﻿public class TreeNode
+﻿BinaryTree binaryTree = new BinaryTree();
+binaryTree.Insert(5);
+binaryTree.Insert(6);
+binaryTree.Insert(7);
+binaryTree.Insert(1);
+binaryTree.Insert(2);
+binaryTree.Insert(3);
+
+//binaryTree.Traverse(binaryTree.root);
+binaryTree.TraverseInOrder(binaryTree.root);
+
+
+public class TreeNode
 {
     public int val;
     public TreeNode left;
@@ -13,7 +25,7 @@
 
 public class BinaryTree
 {
-    private TreeNode root;
+    public TreeNode root;
     public BinaryTree()
     {
         root = null;
@@ -27,10 +39,77 @@ public class BinaryTree
         }
         else
         {
-            while(root != null)
+            InsertRecord(root, val);
+        }
+    }
+
+    private void InsertRecord(TreeNode root, int val)
+    {
+        if(val < root.val)
+        {
+            if(root.left == null)
             {
-                root = root.left;
+                root.left = new TreeNode(val);
+            }
+            else
+            {
+                InsertRecord(root.left, val);
+            }
+        }
+        else
+        {
+            if (root.right == null)
+            {
+                root.right = new TreeNode(val);
+            }
+            else{
+                InsertRecord(root.right, val);
             }
         }
     }
+
+    public void Traverse(TreeNode root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        Traverse(root.left);
+        Console.WriteLine(root.val);
+        Traverse(root.right);
+    }
+
+    public void TraverseInOrder(TreeNode root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        else
+        {
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while(queue.Count > 0)
+            {
+                int levelCount = queue.Count;
+                for(int i = 0; i< levelCount; i++)
+                {
+                    TreeNode currentNode = queue.Dequeue();
+                    if (currentNode.left != null)
+                    {
+                        queue.Enqueue(currentNode.left);
+                    }
+                    if(currentNode.right != null)
+                    {
+                        queue.Enqueue(currentNode.right);
+                    }
+                    Console.WriteLine(currentNode.val);
+                }
+            }
+
+        }
+    }
 }
+
+
+
--- LeetCodeProblems/BinaryTree/obj/Debug/net8.0/BinaryTree.AssemblyInfo.cs
+++ LeetCodeProblems/BinaryTree/obj/Debug/net8.0/BinaryTree.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BinaryTree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faedec84238f9e3a67ade909c8cb168cf20788f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7394a4cf060329af1c044b836838b730804925c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BinaryTree")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BinaryTree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
