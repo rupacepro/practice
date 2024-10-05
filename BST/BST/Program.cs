6305292ea9796a2284840c4d8de22e7c2ccb8276
rupacepro <rup.ace.pasa@gmail.com>
2024-10-04 22:46:31 -0400

    initial

---
+++ BST/BST/Program.cs
@@ -0,0 +1,69 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        BSTLevel bst = new BSTLevel();
+
+        int[] values = { 10, 5, 3, 15, 7, 12, 18 };
+        foreach (int value in values)
+        {
+            bst.Insert(new TreeNode(value));
+        }
+
+        bst.Traverse(bst.Root);
+    }
+}
+
+
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
+
+
+public class BSTLevel
+{
+    public TreeNode Root;
+    public void Insert(TreeNode node)
+    {
+        Root = InsertRec(Root, node.val);
+    }
+    public TreeNode InsertRec(TreeNode root, int val)
+    {
+        if (root == null)
+        {
+            root = new TreeNode(val);
+            return root;
+        }
+        if(root.val > val)
+        {
+            root.left = InsertRec(root.left, val);
+        }
+        else
+        {
+            root.right = InsertRec(root.right, val);
+        }
+        return root;
+        
+    }
+    public void Traverse(TreeNode root)
+    {
+        if(root != null)
+        {
+            Traverse(root.left);
+            Console.WriteLine(root.val);
+            Traverse(root.right);
+        }
+    }
+}
+
